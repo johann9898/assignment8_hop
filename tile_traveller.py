417965b2e96fec949260483ce26cbdb73a2dbbc2
Reynir Gunnarsson <refan@Reynirs-Air.lan>
2020-09-17 10:24:27 +0000

    Created move_player and check_location

--- tile_traveller.py
+++ tile_traveller.py
@@ -4,8 +4,8 @@ when we make a functions that goes around in the grid (3x3)
 if we go to the grid that we cannot go, we get error messege
 otherwise we move.
 """
-startgrid_x = 1
-startgrid_y = 1
+grid_x = 1
+grid_y = 1
 
 def grid_generator(x,y):    
     gridx = x
@@ -19,7 +19,50 @@ def grid_generator(x,y):
             gridy -=1
         elif gridy < 1:
             gridy += 1
-        return gridx, gridy
+    return gridx, gridy
+
+def move_player(direction):
+    if direction.lower() == 'n':
+        grid_generator(grid_x,grid_y+1)
+    elif direction.lower() == 's':
+        grid_generator(grid_x, grid_y-1)
+    elif direction.lower() == 'w':
+        grid_generator(grid_x-1,grid_y)
+    elif direction.lower() == 'e':
+        grid_generator(grid_x+1,grid_y)
+    
+    
+
+def check_location(x,y):
+    if x == 1:
+        if y == 1:
+            print('You can travel: (N)orth.')
+        elif y == 2:
+            print('You can travel: (N)orth or (E)ast or (S)outh')
+        elif y == 3:
+            print('You can travel: (E)ast or (S)outh')
+    
+    elif x == 2:
+        if y == 1:
+            print('You can travel: (N)orth.')
+        elif y == 2:
+            print('You can travel: (S)outh or (W)est.')
+        elif y == 3:
+            print('You can travel: (E)ast or (W)est.')
+    
+    elif x == 3:
+        if y == 1:
+            print('Victory!')
+        elif y == 2:
+            print('You can travel: (N)orth or (S)outh.')
+        elif y == 3:
+            print('You can travel: (E)ast or (S)outh.')
+
+
+
+
+grid_generator(startgrid_x, startgrid_y)
+
 
 print("You can travel: (N)orth.")
 direction = input("Direction: ")
